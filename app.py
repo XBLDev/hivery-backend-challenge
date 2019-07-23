a15121421640213d95c8193fb1ce9275aafef623
XBLDev <u5260839@anu.edu.au>
2019-07-23 21:34:05 +1000

    some fixes

--- app.py
+++ app.py
@@ -17,18 +17,18 @@ def companyemployees():
             if len(companieswithname) > 0:
                 companyindex = companieswithname[0]['index']
         if companyindex == -1:
-            return 'No company with name: ' + name + ' was found, please make sure you are requesting an existing company'
+            return {'employees': []}
         else:
             with open('./resources/people.json') as json_file:
                 data = json.load(json_file)
                 companyemployees = [employee for employee in data if employee['company_id'] == companyindex]
                 if len(companyemployees) == 0:
-                    return 'Company: ' + name + ' has no employee'
+                    return {'employees': []}
                 else:
                     employee_names = [employee['name'] for employee in companyemployees]
-                    return 'Company: ' + name + ' has employees: ' + ', '.join(employee_names)
+                    return {'employees': employee_names}
     else:
-        return 'Please provide the name of at least one company'
+        return {'employees': []}
     
 #Given 2 people, provide their information (Name, Age, Address, phone) 
 #and the list of their friends in common which have brown eyes and are still alive.
@@ -36,28 +36,18 @@ def companyemployees():
 def commonfriends():
     person1_name = request.args.get('person1')
     person2_name = request.args.get('person2')
-    if person1_name is not None and person2_name is not None:
-        with open('./resources/people.json') as json_file:
-            data = json.load(json_file)
-            person1 = [person for person in data if person['name'] == person1_name]
-            person2 = [person for person in data if person['name'] == person2_name]
-            if len(person1) == 0:
-                return 'No person with name: ' + person1_name + ' was found'
-            if len(person2) == 0:
-                return 'No person with name: ' + person2_name + ' was found'
-            if person1[0]['friends'] is not None and person2[0]['friends'] is not None and len(person1[0]['friends']) != 0 and len(person2[0]['friends']) != 0:
-                person1_friends_indexes = [friendindex['index'] for friendindex in person1[0]['friends'] if friendindex['index'] != person1[0]['index']]
-                person2_friends_indexes = [friendindex['index'] for friendindex in person2[0]['friends'] if friendindex['index'] != person2[0]['index']]
-                common_friends_indexes = [index for index in person1_friends_indexes if index in person2_friends_indexes]
-                common_friends_browneyes_alive = [friend for friend in data if friend['index'] in common_friends_indexes and friend['has_died'] == False and friend['eyeColor'] == 'brown']
-                person1_details = {'Name': person1[0]['name'], 'Age': person1[0]['age'], 'Address': person1[0]['address'], 'Phone': person1[0]['phone']}
-                person2_details = {'Name': person2[0]['name'], 'Age': person2[0]['age'], 'Address': person2[0]['address'], 'Phone': person2[0]['phone']}
-                common_friends_names = [friend['name'] for friend in common_friends_browneyes_alive]
-                return {'person1_details': person1_details, 'person2_details': person2_details, 'common_friends_names': common_friends_names}
-            else:
-                return 'No common friends'
-    else:
-        return 'Please provide the names of 2 people'
+    with open('./resources/people.json') as json_file:
+        data = json.load(json_file)
+        person1 = [person for person in data if person['name'] == person1_name] if person1_name is not None else {}
+        person2 = [person for person in data if person['name'] == person2_name] if person2_name is not None else {}
+        person1_details = {'Name': person1[0]['name'], 'Age': person1[0]['age'], 'Address': person1[0]['address'], 'Phone': person1[0]['phone']} if len(person1) > 0 else {}
+        person2_details = {'Name': person2[0]['name'], 'Age': person2[0]['age'], 'Address': person2[0]['address'], 'Phone': person2[0]['phone']} if len(person2) > 0 else {}
+        person1_friends_indexes = [friendindex['index'] for friendindex in person1[0]['friends'] if friendindex['index'] != person1[0]['index']] if len(person1) > 0 and person1[0]['friends'] is not None else []
+        person2_friends_indexes = [friendindex['index'] for friendindex in person2[0]['friends'] if friendindex['index'] != person2[0]['index']] if len(person2) > 0 and person2[0]['friends'] is not None else []
+        common_friends_indexes = [index for index in person1_friends_indexes if index in person2_friends_indexes]
+        common_friends_browneyes_alive = [friend for friend in data if friend['index'] in common_friends_indexes and friend['has_died'] == False and friend['eyeColor'] == 'brown']
+        common_friends_names = [friend['name'] for friend in common_friends_browneyes_alive]
+        return {'person1_details': person1_details, 'person2_details': person2_details, 'common_friends_names': common_friends_names}
 
 #Given 1 people, provide a list of fruits and vegetables they like. 
 # This endpoint must respect this interface for the output: 
@@ -72,11 +62,30 @@ def fruitsvegetables():
             if len(person) == 0:
                 return {"username": "", "age": "", "fruits": [], "vegetables": []}
             if person[0]['favouriteFood'] is not None:
-                return {"username": person[0]['name'], "age": person[0]['age'], "fruits": ["banana", "apple"], "vegetables": ["beetroot", "lettuce"]}
+                person_fruits = [fruit for fruit in person[0]['favouriteFood'] if fruit in ['orange', 'apple', 'banana', 'strawberry']]
+                person_vegetables = [vegetable for vegetable in person[0]['favouriteFood'] if vegetable in ['cucumber', 'beetroot', 'carrot', 'celery']]
+                return {"username": person[0]['name'], "age": person[0]['age'], "fruits": person_fruits, "vegetables": person_vegetables}
             else:
                 return {"username": "", "age": "", "fruits": [], "vegetables": []}
     else:
         return {"username": "", "age": "", "fruits": [], "vegetables": []}
+
+#I don't really know how to seperate fruit from vegetables in this case, so this
+#api is just to get all the unique food, the list is:
+#['orange', 'apple', 'banana', 'strawberry', 'cucumber', 'beetroot', 'carrot', 'celery']
+#with fruits being: 'orange', 'apple', 'banana', 'strawberry', 'cucumber'
+#with vegetables being: 'cucumber', 'beetroot', 'carrot', 'celery'
+@app.route('/getallfood')
+def getallfood():
+    with open('./resources/people.json') as json_file:
+        data = json.load(json_file)
+        allfood = []
+        for person in data:
+            if person['favouriteFood'] is not None:
+                for food in person['favouriteFood']:
+                    if food not in allfood:
+                        allfood.append(food)
+        return {'allfood': allfood}
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test.py
+++ test.py
@@ -8,49 +8,63 @@ class Tests(object):
     def test_companyemployees(self):
         #Tests for /companyemployees
         response_nocompanyname = app.test_client().get(url+ '/companyemployees')
-        data_nocompanyname = response_nocompanyname.get_data(as_text=True)
-        assert data_nocompanyname == 'Please provide the name of at least one company'
+        data_nocompanyname = json.loads(response_nocompanyname.get_data(as_text=True))
+        assert len(data_nocompanyname['employees']) == 0
 
         response_wrongcompanyname = app.test_client().get(url + '/companyemployees?name=adsfadsfsad')
-        data_wrongcompanyname = response_wrongcompanyname.get_data(as_text=True)
-        assert data_wrongcompanyname == 'No company with name: adsfadsfsad was found, please make sure you are requesting an existing company'
+        data_wrongcompanyname = json.loads(response_wrongcompanyname.get_data(as_text=True))
+        assert len(data_wrongcompanyname['employees']) == 0
 
         response_companynoemployee = app.test_client().get(url + '/companyemployees?name=NETBOOK')
-        data_companynoemployee = response_companynoemployee.get_data(as_text=True)
-        assert data_companynoemployee == 'Company: NETBOOK has no employee'
+        data_companynoemployee = json.loads(response_companynoemployee.get_data(as_text=True))
+        assert len(data_companynoemployee['employees']) == 0
 
         response_companymanyemployees = app.test_client().get(url + '/companyemployees?name=STRALUM')
-        data_companymanyemployees = response_companymanyemployees.get_data(as_text=True)
-        assert data_companymanyemployees == 'Company: STRALUM has employees: Kathleen Clarke, Guy Dunn, Butler Norton, Amelia Benjamin, Florence Gordon, Marguerite Frost, Avis Carter, Margery Brooks, English Nolan, Hansen Dejesus, Emily Taylor'
+        data_companymanyemployees = json.loads(response_companymanyemployees.get_data(as_text=True))
+        assert len(data_companymanyemployees['employees']) == 11
         
         #Tests for /commonfriends
         response_nopeoplenames = app.test_client().get(url + '/commonfriends')
-        data_nopeoplenames = response_nopeoplenames.get_data(as_text=True)
-        assert data_nopeoplenames == 'Please provide the names of 2 people'
+        data_nopeoplenames = json.loads(response_nopeoplenames.get_data(as_text=True))
+        assert data_nopeoplenames['person1_details'] == {} 
+        assert data_nopeoplenames['person2_details'] == {} 
+        assert len(data_nopeoplenames['common_friends_names']) == 0
 
         response_noperson2name = app.test_client().get(url + '/commonfriends?person1=Joy Alvarado')
-        data_noperson2name = response_noperson2name.get_data(as_text=True)
-        assert data_noperson2name == 'Please provide the names of 2 people'
+        data_noperson2name = json.loads(response_noperson2name.get_data(as_text=True))
+        assert data_noperson2name['person1_details']['Name'] == 'Joy Alvarado' 
+        assert data_noperson2name['person2_details'] == {} 
+        assert len(data_noperson2name['common_friends_names']) == 0
 
         response_noperson1name = app.test_client().get(url + '/commonfriends?person2=Joy Alvarado')
-        data_noperson1name = response_noperson1name.get_data(as_text=True)
-        assert data_noperson1name == 'Please provide the names of 2 people'
+        data_noperson1name = json.loads(response_noperson1name.get_data(as_text=True))
+        assert data_noperson1name['person1_details'] == {}
+        assert data_noperson1name['person2_details']['Name'] == 'Joy Alvarado' 
+        assert len(data_noperson1name['common_friends_names']) == 0
 
         response_wrongperson1name = app.test_client().get(url + '/commonfriends?person1=Jdfbgoy Alvaradfdaso&person2=Weeks Knox')
-        data_wrongperson1name = response_wrongperson1name.get_data(as_text=True)
-        assert data_wrongperson1name == 'No person with name: Jdfbgoy Alvaradfdaso was found'
+        data_wrongperson1name = json.loads(response_wrongperson1name.get_data(as_text=True))
+        assert data_wrongperson1name['person1_details'] == {}
+        assert data_wrongperson1name['person2_details']['Name'] == 'Weeks Knox' 
+        assert len(data_wrongperson1name['common_friends_names']) == 0
 
         response_wrongperson2name = app.test_client().get(url + '/commonfriends?person1=Joy Alvarado&person2=Weefdks Knofsdggx')
-        data_wrongperson2name = response_wrongperson2name.get_data(as_text=True)
-        assert data_wrongperson2name == 'No person with name: Weefdks Knofsdggx was found'
+        data_wrongperson2name = json.loads(response_wrongperson2name.get_data(as_text=True))
+        assert data_wrongperson2name['person1_details']['Name'] == 'Joy Alvarado' 
+        assert data_wrongperson2name['person2_details'] == {}
+        assert len(data_wrongperson2name['common_friends_names']) == 0
 
         response_havecommonfriends = app.test_client().get(url + '/commonfriends?person1=Joy Alvarado&person2=Weeks Knox')
         data_havecommonfriends = json.loads(response_havecommonfriends.get_data(as_text=True))
+        assert data_havecommonfriends['person1_details']['Name'] == 'Joy Alvarado' 
+        assert data_havecommonfriends['person2_details']['Name'] == 'Weeks Knox' 
         assert data_havecommonfriends['common_friends_names'][0] == 'Decker Mckenzie'
         assert data_havecommonfriends['common_friends_names'][1] == 'Mindy Beasley'
 
         response_havenocommonfriends = app.test_client().get(url + '/commonfriends?person1=Carmella Lambert&person2=Decker Mckenzie')
         data_havenocommonfriends = json.loads(response_havenocommonfriends.get_data(as_text=True))
+        assert data_havenocommonfriends['person1_details']['Name'] == 'Carmella Lambert' 
+        assert data_havenocommonfriends['person2_details']['Name'] == 'Decker Mckenzie' 
         assert len(data_havenocommonfriends['common_friends_names']) == 0
 
         #Tests for /fruitsvegetables
@@ -72,5 +86,19 @@ class Tests(object):
         data_rightpersonname = json.loads(response_rightpersonname.get_data(as_text=True))
         assert data_rightpersonname['age'] == 61
         assert data_rightpersonname['username'] == 'Carmella Lambert'
-        assert len(data_rightpersonname['fruits']) == 2
-        assert len(data_rightpersonname['vegetables']) == 2
+        assert len(data_rightpersonname['fruits']) == 4
+        assert len(data_rightpersonname['vegetables']) == 0
+
+        response_rightpersonname = app.test_client().get(url + '/fruitsvegetables?person=Bonnie Bass')
+        data_rightpersonname = json.loads(response_rightpersonname.get_data(as_text=True))
+        assert data_rightpersonname['age'] == 54
+        assert data_rightpersonname['username'] == 'Bonnie Bass'
+        assert len(data_rightpersonname['fruits']) == 3
+        assert len(data_rightpersonname['vegetables']) == 1
+
+        response_rightpersonname = app.test_client().get(url + '/fruitsvegetables?person=Grace Kelly')
+        data_rightpersonname = json.loads(response_rightpersonname.get_data(as_text=True))
+        assert data_rightpersonname['age'] == 24
+        assert data_rightpersonname['username'] == 'Grace Kelly'
+        assert len(data_rightpersonname['fruits']) == 1
+        assert len(data_rightpersonname['vegetables']) == 3
